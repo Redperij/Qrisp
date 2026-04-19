31c17bd64315074cfcebb897feee7da4f31c86b7
RedHawk <tadshig@gmail.com>
2026-04-19 10:51:02 +0000

    Modify foqcs_lcu_prep test

--- tests/block_encodings_tests/test_block_encoding_foqcs_lcu.py
+++ tests/block_encodings_tests/test_block_encoding_foqcs_lcu.py
@@ -19,5 +19,123 @@
 import numpy as np
 from qrisp.block_encodings import BlockEncoding
 
+import numpy as np
+from qrisp.block_encodings import BlockEncoding
+from qrisp import QuantumVariable
+from qrisp.block_encodings import foqcs_prep_heisenberg_1D
+
 def test_block_encoding_from_foqcs_lcu_prep():
-    pass
+    """
+    TODO: DOC
+    """
+
+    # Initialize variables + their values
+    L = 4
+    g = np.array(np.random.uniform(-1, 1, 3), dtype="complex")
+    J = np.array(np.random.uniform(-1, 1, 3), dtype="complex")
+
+    # Normalize
+    norm = np.linalg.norm(np.block([g, J]))
+    g /= norm
+    J /= norm
+
+    heis_g = {"X": g[0], "Y": g[1], "Z": g[2]}
+    heis_J = {"X": J[0], "Y": J[1], "Z": J[2]}
+
+
+    # Prep base state using qv + Do FOQCS-LCU magic using Daniil's function
+    state = foqcs_prep_heisenberg_1D(L, heis_g, heis_J)
+    sv = state.qs.statevector("function")
+
+    # Reverse bit order (why?) + Simulate resulting quantum state
+    # var_prep[:].reverse()
+
+    # Build Dicke states, give them all unique weights from g[0] to J[2]: base, double, 2NN, 2NN double... PLACEHOLDER FOR NOW, IF IT DOESN'T HOLD UP REPLACE IT
+    dicke_1_norm = 1 / (np.sqrt(L))
+    dicke_1 = np.zeros(2**L)
+    for i in range(L):
+        dicke_1[2**i] = dicke_1_norm
+
+    dicke_double = np.zeros(4**L)
+    for i in range(L):
+        dicke_double[2**i + 2 ** (L + i)] = dicke_1_norm
+
+    dicke_2NN_norm = 1 / (np.sqrt(L - 1))
+    dicke_2NN = np.zeros((2**L,), dtype="complex")
+    for i in range(L - 1):
+        dicke_2NN[2**i + 2 ** (i + 1)] = dicke_2NN_norm
+
+    dicke_2NN_double = np.zeros((4**L,), dtype="complex")
+    for i in range(L - 1):
+        dicke_2NN_double[
+            2**i + 2 ** (i + 1) + 2 ** (i + L) + 2 ** (i + L + 1)
+        ] = dicke_2NN_norm
+
+    ref_state = np.zeros((2 ** (6 + 2 * L),), dtype="complex")
+    zero_n = np.array([1] + [0] * (2**L - 1))
+    non_zero_states = []
+
+    # g[0]
+    ref_state += g[0] * np.kron(
+            [1 if i == 2 ** (6 - 1) else 0 for i in range(2**6)],
+            np.kron(dicke_1, zero_n),
+        )
+    
+    # g[1]
+    ref_state += g[1] * np.kron(
+        [1 if i == 2 ** (6 - 2) else 0 for i in range(2**6)], dicke_double
+    )
+    
+    # g[2]
+    ref_state += g[2] * np.kron(
+        [1 if i == 2 ** (6 - 3) else 0 for i in range(2**6)],
+        np.kron(zero_n, dicke_1),
+    )
+
+    # J[0]
+    ref_state += J[0] * np.kron(
+        [1 if i == 2 ** (6 - 4) else 0 for i in range(2**6)],
+        np.kron(dicke_2NN, zero_n),
+    )
+
+    # J[1]
+    ref_state += J[1] * np.kron(
+        [1 if i == 2 ** (6 - 5) else 0 for i in range(2**6)], dicke_2NN_double
+    )
+
+    # J[2]
+    ref_state += J[2] * np.kron(
+        [1 if i == 2 ** (6 - 6) else 0 for i in range(2**6)],
+        np.kron(zero_n, dicke_2NN),
+    )
+
+    print(state.qs)
+
+    print("State:")
+    for i in range(2**14):
+        bits = format(i, "014b")
+        amp = sv({state: bits})
+        if np.isclose(amp, 0j, atol=1e-6) == False: #np.real(amp):
+            print(bits, amp)
+
+    print()
+    print("Ref state:")
+    init_ref = {}
+    q = 0
+    for i in ref_state:
+        bits = format(q, "014b")
+        if i != 0:
+            init_ref[bits] = i
+            print(bits, i)
+        q += 1
+
+    #print(init_ref)
+
+    #expected = QuantumVariable(14)
+    #expected.init_state(init_ref, method="qiskit")
+
+    #print(f"State:\n{state}")
+    #print(f"Expected state:\n{expected}")
+
+    # Comparison of expected and actual states
+    assert sv == ref_state
